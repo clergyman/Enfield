886b0bbd47aa2fcfa8f99874672b1946cdabda61
Clergyman <clergyman@yandex.ru>
2014-11-28 12:36:59 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Enfield::Application.config.secret_key_base = 'ead257654d2a21e7c86d351a328147c77637a2092031ef35d9b44be95eff3bf4313f4af189ad3fc7217f14c40c75159d4a9823f2556aec6e31bca89a61515c27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Enfield::Application.config.session_store :cookie_store, key: '_enfield_session'
