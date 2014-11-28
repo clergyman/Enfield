ad42f4edf5c20eca56b32b7492443a88f3d4d6d4
Clergyman <clergyman@yandex.ru>
2014-11-28 15:27:59 +0300

    secret token is now generated dynamically

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Enfield::Application.config.secret_key_base = 'ead257654d2a21e7c86d351a328147c77637a2092031ef35d9b44be95eff3bf4313f4af189ad3fc7217f14c40c75159d4a9823f2556aec6e31bca89a61515c27'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Enfield::Application.config.secret_key_base = secure_token
