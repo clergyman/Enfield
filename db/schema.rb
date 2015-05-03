883270bff58c3e4f7e1178b2202aec5e7e17787b
Clergyman <clergyman@yandex.ru>
2015-05-03 13:28:27 +0300

    fized migration problems

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501140600) do
+ActiveRecord::Schema.define(version: 20150503093445) do
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
