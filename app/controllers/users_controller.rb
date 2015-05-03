2be8030d10e736fc4c3907a34c8c5343beab2a70
Clergyman <clergyman@yandex.ru>
2015-05-03 20:39:20 +0300

    added registration support

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "New user has been sucessfully created!"
       redirect_to @user
     else
       render 'new'
