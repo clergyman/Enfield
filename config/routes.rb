70d1d084adb746c2e21a12056b808c4d64d22fa9
Clergyman <clergyman@yandex.ru>
2015-05-03 12:26:39 +0300

    added new user route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Enfield::Application.routes.draw do
   get "users/new"
 
   root  'static_pages#home'
+  match '/adduser',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contacts', to: 'static_pages#contacts', via: 'get'
--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141128114951) do
-
-  create_table "tasks", force: true do |t|
-    t.string   "description"
-    t.integer  "user_id"
-    t.integer  "phase_id"
-    t.string   "history"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20150501140600) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
